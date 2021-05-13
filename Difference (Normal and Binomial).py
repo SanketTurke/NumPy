f7ecbc909bb01bb9e0eac95cc163d5d8b503dcde
SanketTurke <84033038+SanketTurke@users.noreply.github.com>
2021-05-13 13:28:31 +0530

    Add files via upload

---
+++ Difference (Normal and Binomial).py
@@ -0,0 +1,8 @@
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.normal(loc=50, scale=5, size=1000), hist=False, label='normal')
+sns.distplot(random.binomial(n=100, p=0.5, size=1000), hist=False, label='binomial')
+
+plt.show()
